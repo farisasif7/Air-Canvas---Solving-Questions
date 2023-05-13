c19ec956e754c7a27adbee57b27a74a2d33beaa5
OldHitman7 <farisbinasif7@gmail.com>
2023-05-13 11:16:15 +0500

    removed answer window and added solution function

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import model_from_json
 import tkinter as tk
+import sympy
+
+def eq_sympy(equation_str):
+  try:
+      solutions = eval(equation_str)
+      print("Solution to this equation:", solutions)
+  except :
+      print("Sorry, could not solve this equation.")
 
 chars = []
 labels = ['%', '*', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '[', ']']
@@ -72,32 +80,10 @@ def function_other():
     eq=""
     for i in chars:
       eq += str(labels[i])
-    print(eq)
+    print("The detected equation: ", eq)
+    eq_sympy(eq)
     #empty chars
     chars.clear()
-
-def makeWindow():
-    # Create the root window
-    root = tk.Tk()
-    root.geometry("500x500")
-
-    # Create a frame to hold the labels
-    frame = tk.Frame(root)
-
-    # Create two labels with the text we want to display
-    label1 = tk.Label(frame, text="Line 1", font=("Arial", 18), justify="center", pady=50)
-    label2 = tk.Label(frame, text="Line 2", font=("Arial", 18), justify="center", pady=50)
-
-    # Pack the labels into the frame, with some padding
-    label1.pack()
-    label2.pack()
-
-    # Pack the frame into the root window, centering it horizontally and vertically
-    frame.pack(fill="both", expand=True)
-    root.after(10000, root.destroy)
-    
-    # Start the main event loop
-    root.mainloop()
 
 # create a directory to save the screenshots
 dir_name = 'screenshots'
@@ -231,7 +217,6 @@ while True:
                 paintWindow[67:,:,:] = 255  
                 function()
                 function_other()
-                # makeWindow()
         else :
             if colorIndex == 0:
                 bpoints[black_index].appendleft(center)
